763b4559269d49dcdd93f15692a042cd1374350d
conashin <41155840+conashin@users.noreply.github.com>
2022-12-31 23:56:13 +0800

    Update app.js

--- app.js
+++ app.js
@@ -54,3 +54,9 @@ async function handleEvent(event) {
   // use reply API
   return client.replyMessage(event.replyToken, echo);
 }
+
+  // listen on port
+ const port = process.env.PORT || 3000;
+ app.listen(port, () => {
+   console.log(`listening on ${port}`);
+ });
